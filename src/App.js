85400c2d6459ae6e06996dfd03b513f4db12072c
Tracy Lee <tracy@thisdot.co>
2017-10-23 03:22:56 -0700

    okay done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import styled from 'styled-components/native';
 import { TabNavigator } from 'react-navigation';
+import ReactJeopardy from './ReactJeopardy';
+import AngularJeopardy from './AngularJeopardy';
 
 const Container = styled.View`
   justifyContent: center;
@@ -25,6 +27,14 @@ export const mainNavigator = TabNavigator({
   Home: {
     screen: App,
     path: ''
+  },
+  React: {
+    screen: ReactJeopardy,
+    path: 'react'
+  },
+  Angular: {
+    screen: AngularJeopardy,
+    path: 'angular'
   }
 });
 
