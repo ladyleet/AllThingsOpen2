3e100f7e758e84b70f06e00f114310cc611e3219
Tracy Lee <tracy@thisdot.co>
2017-10-22 17:56:45 -0700

    initial commit

---
+++ src/ReactJeopardy.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView
+} from 'react-native';
+import styled from 'styled-components/native';
+
+const data = [{
+  Q: "The company that created React.",
+  A: "What is Facebook?"
+},{
+  Q: "What happens during the lifecycle of a React component?",
+  A: "Initialization, State/property updates, Destruction"
+},{
+  Q: "True or False: JSX a requirement in React.",
+  A: "What is false?"
+},{
+  Q: "A popular flux framework.",
+  A: "What is Redux, Flummox, Alt, Fluxible, etc?"
+},{
+  Q: "A component that is a function of the properties provided to it.",
+  A: "What are stateless components?"
+},{
+  Q: "True or False: React is an MVC based framework.",
+  A: "What is false?"
+},{
+  Q: "_____ needs to be updated to achieve dynamic UI updates.",
+  A: "What is state?"
+},{
+  Q: "The API necessary for every React component.",
+  A: "What is render?"
+},{
+  Q: "React focuses on this part of MVC.",
+  A: "What is view?"
+},{
+  Q: "The name of the CLI used to create react apps.",
+  A: "What is create-react-app?"
+}];
+
+const Container = styled.View`
+  margin: 10px;
+`;
+
+const TextStyled = styled.Text`
+  fontSize: 20;
+  textAlign: center;
+  margin: 10px;
+`;
---
+++ src/VueJeopardy.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView
+} from 'react-native';
+import styled from 'styled-components/native';
+
+const data = [{
+  Q: "The creator of Vue.",
+  A: "Who is Evan You?"
+},{
+  Q: "True or False: Vue has a cli.",
+  A: "What is true?"
+},{
+  Q: "This country has the largest adoption of Vue.",
+  A: "What is China?"
+},{
+  Q: "Vue has over _____ stars on Github.",
+  A: "What is 56,635?"
+},{
+  Q: "Vue was written while the author was at this company.",
+  A: "What is Google?"
+},{
+  Q: "True or False: React is an MVC based framework.",
+  A: "What is false?"
+},{
+  Q: "Vue was released on this year.",
+  A: "What is February 2014?"
+}];
+
+const Container = styled.View`
+margin: 10px;
+`;
+
+const TextStyled = styled.Text`
+fontSize: 20;
+textAlign: center;
+margin: 10px;
+`;
+
